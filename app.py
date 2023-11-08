173bd8eb9da798665a16318ebeef567cd917686a
EdagPSIT <134361096+EdagPSIT@users.noreply.github.com>
2023-11-08 04:55:43 +0000

    Build app and index page

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import pyjokes
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   joke = pyjokes.get_joke()
+   return render_template("index.html", joke=joke)
+
+if __name__ == "__main__":
+   app.run(debug=True,port=5001)
